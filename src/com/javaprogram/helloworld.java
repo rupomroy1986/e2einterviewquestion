b2b07e9eaa17690e2a71676ce7d6bb62d9dc7ccd
roy.rupom6@gmail.com <roy.rupom6@gmail.com>
2020-06-25 16:53:48 +0530

    java1

---
+++ src/com/javaprogram/helloworld.java
@@ -0,0 +1,11 @@
+package com.javaprogram;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello india");
+
+	}
+
+}
